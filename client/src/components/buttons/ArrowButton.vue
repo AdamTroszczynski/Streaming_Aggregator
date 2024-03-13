fb9649f52d5baa9d9b0dc1d05a5ab64efcbbe0c8
Amanowsky <adamtroszczynski@outlook.com>
2024-03-14 00:46:16 +0100

    Arrow btn added

---
+++ client/src/components/buttons/ArrowButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <button
+    @click="emitClickEvent()">
+      <RightArrowIcon v-if="isRight"></RightArrowIcon>
+      <LeftArrowIcon v-else></LeftArrowIcon>
+    </button>
+</template>
+
+<script setup lang="ts">
+import LeftArrowIcon from '@/components/icons/arrows/LeftArrowIcon.vue';
+import RightArrowIcon from '@/components/icons/arrows/RightArrowIcon.vue';
+
+defineProps({
+  isRight: {
+    type: Boolean,
+    default: true
+  }
+});
+
+const emit = defineEmits<{
+  /** Emit event after click button */
+  (e: 'clickAction'): void;
+}>();
+
+/** Emit click action event */
+const emitClickEvent = (): void => emit('clickAction');
+</script>
