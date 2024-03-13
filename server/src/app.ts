0878498bfe9c9b3d2c048466931de88b52964700
Kamil-Pawelski <kp02224@gmail.com>
2024-03-13 20:34:55 +0100

    Starting conf

---
+++ server/src/app.ts
@@ -0,0 +1,21 @@
+import express, { Express } from "express";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import cors from "cors";
+import { json } from "body-parser";
+import { BASE_URL } from "./const/commonConst";
+import { BASE_CLIENT_URL } from "./const/commonConst";
+dotenv.config();
+
+const app: Express = express();
+
+app.use(json());
+
+app.use(helmet());
+app.use(cors({ origin: BASE_CLIENT_URL }));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running at ${BASE_URL}:${port}`);
+});
