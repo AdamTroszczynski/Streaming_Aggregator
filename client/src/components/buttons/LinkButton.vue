40301b35cd5b19d911db070fb97efd0fe655fce7
Amanowsky <adamtroszczynski@outlook.com>
2024-03-13 23:05:06 +0100

    action button and link button added

---
+++ client/src/components/buttons/LinkButton.vue
@@ -0,0 +1,45 @@
+<template>
+  <a v-if="isLink" 
+    class="text-white font-medium text-sm 2xl:text-base" 
+    :href="goTo">
+    <slot></slot>
+  </a>
+
+  <button v-else-if="isButton" 
+    class="btn btn-xs 2xl:btn-sm rounded bg-information text-white border-none font-semibold 2xl:text-lg 2xl:px-2
+      hover:bg-information hover:text-white">
+    <slot></slot>
+  </button>
+
+  <RouterLink v-else 
+    class="text-white 2xl:text-lg"
+    :class="isSelect ? 'font-bold underline' : 'font-normal no-underline'"
+    :to="goTo">
+    <slot></slot>
+  </RouterLink>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+
+defineProps({
+  goTo: {
+    type: String,
+    default: '/'
+  },
+  isButton: {
+    type: Boolean,
+    default: false
+  },
+  isLink: {
+    type: Boolean,
+    default: false
+  },
+  isSelect: {
+    type: Boolean,
+    default: false
+  }
+});
+
+
+</script>
