66c7c54e4512d97203acda3c44a3173b64aff9bb
Amanowsky <adamtroszczynski@outlook.com>
2024-03-15 01:40:05 +0100

    remove testing environment

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,10 +1,7 @@
 <template>
-  <main class="w-screen h-screen flex justify-center items-center bg-[#F2F2F2]">
-    <DaysSelector>
-    </DaysSelector>
+  <main>
   </main>
 </template>
 
 <script setup lang="ts">
-import DaysSelector from '@/widgets/DaysSelector.vue';
 </script>
