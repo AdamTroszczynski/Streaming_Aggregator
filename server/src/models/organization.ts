40a10b071983cac03cf866bfd8fd67e170f58181
Kamil-Pawelski <kp02224@gmail.com>
2024-03-15 16:26:29 +0100

    Event and organization model

---
+++ server/src/models/organization.ts
@@ -0,0 +1,39 @@
+export default class Organizer {
+  organizationId: number;
+  formalName: string;
+  displayName: string;
+  country: string;
+  city: string;
+  postalCode: string;
+  streetAddress: string;
+  phoneNumber: string;
+  email: string;
+  websiteLink: string;
+  photo: Blob;
+
+  constructor(
+    organizationId: number,
+    formalName: string,
+    displayName: string,
+    country: string,
+    city: string,
+    postalCode: string,
+    streetAddress: string,
+    phoneNumber: string,
+    email: string,
+    websiteLink: string,
+    photo: Blob
+  ) {
+    this.organizationId = organizationId;
+    this.formalName = formalName;
+    this.displayName = displayName;
+    this.country = country;
+    this.city = city;
+    this.postalCode = postalCode;
+    this.streetAddress = streetAddress;
+    this.phoneNumber = phoneNumber;
+    this.email = email;
+    this.websiteLink = websiteLink;
+    this.photo = photo;
+  }
+}
