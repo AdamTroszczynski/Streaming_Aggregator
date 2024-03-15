2d6879d9236defb647f213bc2fad40e846728710
Amanowsky <adamtroszczynski@outlook.com>
2024-03-15 01:37:12 +0100

    Change Day model, add more DateUtil methods

--- client/src/utils/DateUtil.ts
+++ client/src/utils/DateUtil.ts
@@ -2,15 +2,23 @@ import Day from "@/models/Day";
 
 export default class DateUtil{
 
-  public static getNextDay(day: Date, i: number) {
+  public static getNextDay(day: Date, i: number): Day {
     const newDay = new Date(day);
     newDay.setDate(day.getDate() + i);
     return new Day(newDay);
   }
 
-  public static getPrevDay(day: Date, i: number){
+  public static getPrevDay(day: Date, i: number): Day {
     const newDay = new Date(day);
     newDay.setDate(day.getDate() - i);
     return new Day(newDay);
+  }
+
+  public static generateDateId(date: Date): string {
+    return `${date.getDate()}.${date.getMonth()}.${date.getFullYear()}`;
+  }
+
+  public static getShowingDate(date: Date): string {
+    return `${("0" + date.getDate()).slice(-2)}.${("0" + (date.getMonth()+1)).slice(-2)}.`;
   }
 }
