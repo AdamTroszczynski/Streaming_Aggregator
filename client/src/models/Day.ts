f8016a71da56b628912f51a7f95d30d94c7cf514
Amanowsky <adamtroszczynski@outlook.com>
2024-03-15 16:57:08 +0100

    Move widget logic to widget component, change Day model

--- client/src/models/Day.ts
+++ client/src/models/Day.ts
@@ -1,13 +1,13 @@
 import { nameOfDays } from "@/const/commonConst";
 import DateUtil from "@/utils/DateUtil";
 export default class Day{
-  id: string;
+  id: number;
   name: string;
   showDate: string;
   fullDate: Date;
 
   constructor(fullDate: Date) {
-    this.id = DateUtil.generateDateId(fullDate);
+    this.id = fullDate.getTime();
     this.fullDate = fullDate;
     this.showDate = DateUtil.getShowingDate(fullDate);
     this.name = nameOfDays[fullDate.getDay()];
