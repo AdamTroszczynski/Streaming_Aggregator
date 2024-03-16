179bac0b9ee0e62bc2abb62f7361990d9a5926a0
Amanowsky <adamtroszczynski@outlook.com>
2024-03-16 21:51:28 +0100

    added i18n to navbar

--- client/src/components/buttons/LangButton.vue
+++ client/src/components/buttons/LangButton.vue
@@ -1,15 +1,18 @@
 <template>
   <button @click="toggleLanguage" class="bg-none border-none flex gap-2 items-center justify-center">
-    <p class="text-white font-normal lg:text-sm 2xl:text-base">PL</p>
+    <p class="text-white font-normal lg:text-sm 2xl:text-base">{{ t(`navbar.lang`) }}</p>
     <LanguageIcon></LanguageIcon>
   </button>
 </template>
+
 <script setup lang="ts">
 import LanguageIcon from '../icons/navbar/LanguageIcon.vue';
+import { useI18n } from 'vue-i18n'
+
+const { t, locale } = useI18n({ useScope: 'global'});
 
 /** Switch language in i18n*/
 const toggleLanguage = (): void => {
-  console.log('toggle lang')
-  // todo
-}
+  locale.value = locale.value === 'en' ? 'pl' : 'en';
+};
 </script>
