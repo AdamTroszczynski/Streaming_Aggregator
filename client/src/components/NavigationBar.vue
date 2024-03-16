72c7b8db6d64a9d0415028dead4abfb34a0c8a3e
Amanowsky <adamtroszczynski@outlook.com>
2024-03-16 21:51:28 +0100

    added i18n to navbar

--- client/src/components/NavigationBar.vue
+++ client/src/components/NavigationBar.vue
@@ -3,13 +3,13 @@
     <h2 class="text-2xl font-bold text-black">STREAM 'N GO</h2>
     <ActionButton :isNavibar="true" class="lg:hidden"><MenuIcon></MenuIcon></ActionButton>
     <div class="hidden lg:flex lg:gap-5">
-      <LinkButton :isSelect="true">Strona główna</LinkButton>
-      <LinkButton>Organizacje</LinkButton>
-      <LinkButton>Kontakt</LinkButton>
+      <LinkButton :isSelect="true">{{ t(`navbar.homePage`) }}</LinkButton>
+      <LinkButton>{{ t(`navbar.organizations`) }}</LinkButton>
+      <LinkButton>{{ t(`navbar.contact`) }}</LinkButton>
     </div>
     <div class="hidden lg:flex lg:items-center lg:gap-4">
-      <LinkButton>Logowanie</LinkButton>
-      <LinkButton :isButton="true">Rejestracja</LinkButton>
+      <LinkButton>{{ t(`navbar.login`) }}</LinkButton>
+      <LinkButton :isButton="true">{{ t(`navbar.registration`) }}</LinkButton>
       <LangButton class="lg:ml-5"></LangButton>
     </div>
   </nav>
@@ -20,4 +20,8 @@ import ActionButton from './buttons/ActionButton.vue';
 import MenuIcon from './icons/navbar/MenuIcon.vue';
 import LinkButton from './buttons/LinkButton.vue';
 import LangButton from './buttons/LangButton.vue';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+
 </script>
