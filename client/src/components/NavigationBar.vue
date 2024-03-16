7e872d92b4f915247e9441f16ca4005b2d6a89b8
Amanowsky <adamtroszczynski@outlook.com>
2024-03-16 23:17:49 +0100

    background image, isLogin view and toggleMenu added

--- client/src/components/NavigationBar.vue
+++ client/src/components/NavigationBar.vue
@@ -1,18 +1,48 @@
 <template>
-  <nav class="w-full p-5 flex justify-between items-center border-4 border-black lg:py-3.5 lg:px-14">
-    <h2 class="text-2xl font-bold text-black">STREAM 'N GO</h2>
-    <ActionButton :isNavibar="true" class="lg:hidden"><MenuIcon></MenuIcon></ActionButton>
-    <div class="hidden lg:flex lg:gap-5">
-      <LinkButton :isSelect="true">{{ t(`navbar.homePage`) }}</LinkButton>
-      <LinkButton>{{ t(`navbar.organizations`) }}</LinkButton>
-      <LinkButton>{{ t(`navbar.contact`) }}</LinkButton>
-    </div>
-    <div class="hidden lg:flex lg:items-center lg:gap-4">
-      <LinkButton>{{ t(`navbar.login`) }}</LinkButton>
-      <LinkButton :isButton="true">{{ t(`navbar.registration`) }}</LinkButton>
-      <LangButton class="lg:ml-5"></LangButton>
-    </div>
-  </nav>
+  <div
+    class="h-[140px] bg-[url('/NavbarBgMobile.png')] bg-right-top bg-no-repeat lg:bg-[url('/NavbarBgPc.png')] lg:h-[419px]">
+    <nav class="w-full p-5 flex justify-between items-center lg:py-3.5 lg:px-14 relative">
+      <h2 class="text-2xl font-bold text-black">STREAM 'N GO</h2>
+      <ActionButton @clickAction="toggleMenu" :isNavibar="true" class="lg:hidden">
+        <MenuIcon></MenuIcon>
+      </ActionButton>
+      <div class="hidden lg:flex lg:gap-5 3xl:ml-[300px] 4xl:ml-[500px]">
+        <LinkButton :isSelect="true">{{ t(`navbar.homePage`) }}</LinkButton>
+        <LinkButton>{{ t(`navbar.organizations`) }}</LinkButton>
+        <LinkButton>{{ t(`navbar.contact`) }}</LinkButton>
+      </div>
+      <div class="hidden lg:flex lg:items-center lg:gap-4">
+        <template v-if="isLogin">
+          <LinkButton>{{ t(`navbar.login`) }}</LinkButton>
+          <LinkButton :isButton="true">{{ t(`navbar.registration`) }}</LinkButton>
+        </template>
+        <template v-else>
+          <button
+            :class="avatarBtnStyles">
+            <img src="/avatar.png">
+          </button>
+        </template>
+        <LangButton class="lg:ml-5"></LangButton>
+      </div>
+      <div v-if="isMenuActive"
+        class="absolute w-1/2 bg-decorator shadow-lg flex flex-col gap-2 p-4 items-center top-[95%] left-[25%] rounded-2xl lg:hidden">
+        <LinkButton :isSelect="true">{{ t(`navbar.homePage`) }}</LinkButton>
+        <LinkButton>{{ t(`navbar.organizations`) }}</LinkButton>
+        <LinkButton>{{ t(`navbar.contact`) }}</LinkButton>
+        <template v-if="isLogin">
+          <LinkButton>{{ t(`navbar.login`) }}</LinkButton>
+          <LinkButton :isButton="true">{{ t(`navbar.registration`) }}</LinkButton>
+        </template>
+        <template v-else>
+          <button
+            :class="avatarBtnStyles">
+            <img src="/avatar.png">
+          </button>
+        </template>
+        <LangButton class="lg:ml-5"></LangButton>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -21,7 +51,20 @@ import MenuIcon from './icons/navbar/MenuIcon.vue';
 import LinkButton from './buttons/LinkButton.vue';
 import LangButton from './buttons/LangButton.vue';
 import { useI18n } from 'vue-i18n';
+import { computed, ref, type Ref } from 'vue';
 
 const { t } = useI18n();
+const isLogin = ref(true);
+
+/** Side menu active status */
+const isMenuActive: Ref<boolean> = ref(false);
+
+/** Toggle side menu active status */
+const toggleMenu = (): void => {
+  isMenuActive.value = !isMenuActive.value;
+};
+
+/** Style classes for AvatarBtn */
+const avatarBtnStyles = `btn btn-circle h-[37px] w-[37px] min-h-0 p-0 border-none bg-information rounded-full flex justify-center items-center hover:bg-information`
 
 </script>
