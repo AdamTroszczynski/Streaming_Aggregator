2843110991470b1b6f7134c37a6bd1e028ea3b72
Amanowsky <adamtroszczynski@outlook.com>
2024-03-18 17:27:06 +0100

    Added event generate logic

--- client/src/stores/eventsStore.ts
+++ client/src/stores/eventsStore.ts
@@ -2,7 +2,7 @@ import { ref, type Ref } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useEventsStore = defineStore('eventsStore', () => {
-  const selectedDay: Ref<Number> = ref(0);
+  const selectedDay: Ref<number> = ref(0);
 
   return { selectedDay };
 })
