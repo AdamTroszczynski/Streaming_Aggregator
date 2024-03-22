c581c5c013d090c772f4cbdc5ea478d3e7d8f3f9
Kamil-Pawelski <kp02224@gmail.com>
2024-03-22 10:21:23 +0100

    Event's model to type

---
+++ server/src/types/Event.ts
@@ -0,0 +1,21 @@
+type Event = {
+  eventId: number;
+  eventName: string;
+  tag: string;
+  language: string;
+  streamingLink: string;
+  startDate: number;
+  endDate: number;
+  organizationFormalName: string;
+  organizationDisplayName: string;
+  country: string;
+  city: string;
+  postalCode: string;
+  streetAddress: string;
+  phoneNumber: string;
+  email: string;
+  websiteLink: string;
+  photo: Blob;
+}
+
+export default Event;
