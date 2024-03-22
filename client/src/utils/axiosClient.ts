195540455d2e84361cdac1b5cf48e4c381bad4d8
Amanowsky <adamtroszczynski@outlook.com>
2024-03-22 22:25:39 +0100

    axios Instance added, Event type added

--- client/src/utils/axiosClient.ts
+++ client/src/utils/axiosClient.ts
@@ -1,1 +1,11 @@
 import axios from 'axios';
+
+const eventFetch = axios.create({
+  baseURL: 'http://localhost:8080/api/',
+  timeout: 1000,
+  headers: {
+    Accept: 'application/json'
+  },
+});
+
+export default eventFetch
