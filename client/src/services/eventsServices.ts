fa22571e3a46903d1286eb3e48276e3b3fb69992
Amanowsky <adamtroszczynski@outlook.com>
2024-03-23 16:31:39 +0100

    axios instance, event type and temp eventService added

--- client/src/services/eventsServices.ts
+++ client/src/services/eventsServices.ts
@@ -1,17 +1,18 @@
-import axios from "axios";
-import eventFetch from "@/utils/axiosClient";
-import type {Event} from "@/types/commonTypes";
-
+import axios from 'axios';
+import eventFetch from '@/utils/axiosClient';
+import type { Event } from '@/types/commonTypes';
 
 /** Get all day's events based on day's timestamp
  * @returns {Promise<{[key: string]: Event[]} | void>}
-*/
-export const getEventsPreview = async (timestamp: number): Promise<{[key: string]: Event[]} | void> => {
+ */
+export const getEventsPreview = async (
+  timestamp: number,
+): Promise<{ [key: string]: Event[] } | void> => {
   try {
-    const response = await eventFetch('/...', {data: {timestamp}});
+    const response = await eventFetch('/...', { data: { timestamp } });
     const data = response.data;
     return data;
-  }catch(err){
+  } catch (err) {
     console.log(err);
   }
-}
+};
