735a22ff4f2c78568ed17bc387dee54a5a154899
Amanowsky <adamtroszczynski@outlook.com>
2024-03-28 14:56:24 +0100

    bug fix

--- client/src/services/eventsServices.ts
+++ client/src/services/eventsServices.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
 import axiosClient from '@/utils/axiosClient';
-import type { EventsArrays } from '@/types/commonTypes';
+import type { EventsPrevArrays } from '@/types/commonTypes';
 import type { Event } from '@/types/Event';
 
 /** Get all day's events based on day's timestamp
  * @returns {Promise<{[key: string]: Event[]} | void>}
  */
-export const getEventsPreview = async (timestamp: number): EventsArrays => {
+export const getEventsPreview = async (timestamp: number): EventsPrevArrays => {
   try {
     const response = await axiosClient.get('/event/events', {
       data: timestamp,
