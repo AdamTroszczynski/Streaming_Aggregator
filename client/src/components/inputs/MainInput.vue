da8b4d39417d42f98a175ddf641b64b640885435
Amanowsky <adamtroszczynski@outlook.com>
2024-04-25 13:34:27 +0200

    MainInput mobile added

---
+++ client/src/components/inputs/MainInput.vue
@@ -0,0 +1,49 @@
+<template>
+  <label class="form-control w-full">
+    <div v-if="label" class="label pb-1">
+      <span class="label-text text-sm font-medium text-navy"
+        >{{ label }} <span v-if="isRequired" class="text-red">*</span></span
+      >
+    </div>
+    <input
+      v-model="value"
+      :name="name"
+      :type="type"
+      :placeholder="placeholder"
+      class="input input-bordered input-sm w-full border-inputGrey bg-white text-black focus:border-inputGrey"
+    />
+    <div v-if="errorMessage">
+      <span class=""> - {{ errorMessage }} </span>
+    </div>
+  </label>
+</template>
+
+<script setup lang="ts">
+import { useField } from 'vee-validate';
+
+/** Catch value and errors from field (input component) */
+const { errorMessage, value } = useField<string>(() => props.name);
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  label: {
+    type: String,
+    required: false,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  placeholder: {
+    type: String,
+    default: '',
+  },
+  isRequired: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
---
+++ client/src/views/TestView.vue
@@ -0,0 +1,24 @@
+<template>
+  <main class="flex min-h-screen w-full flex-col bg-[#F2F2F2]">
+    <NavigationBar />
+
+    <div class="flex w-full flex-col items-center px-6">
+      <MainInput
+        :name="'companyName'"
+        :type="'text'"
+        :isRequired="true"
+        :label="'Nazwa Firmy'"
+      ></MainInput>
+    </div>
+  </main>
+</template>
+<script setup lang="ts">
+import NavigationBar from '@/components/common/NavigationBar.vue';
+import DaySelector from '@/widgets/DaySelector.vue';
+import EventsList from '@/widgets/EventsList.vue';
+import MainFooter from '@/components/common/MainFooter.vue';
+import MainInput from '@/components/inputs/MainInput.vue';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+</script>
